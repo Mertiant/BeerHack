d0ecae798ec44f254d64b3048392826ab1ab9045
Burstinfinity <johnxue88@yahoo.com>
2016-04-16 23:30:00 -0400

    added new page

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,9 @@ function startScan() {
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
             resultDiv.innerHTML = s;
+            if (result.text=="786150000052") {
+                window.navigate("productPage.html") 
+            }
         },
         function (error) {
             alert("Scanning failed: " + error);
