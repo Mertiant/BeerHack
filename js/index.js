3a8fc09874a0448809dfd9bdea5dda978fa36b8d
Burstinfinity <johnxue88@yahoo.com>
2016-04-17 07:22:43 -0400

    search

---
+++ js/index.js
@@ -0,0 +1,59 @@
+var resultDiv;
+var data = { 
+             beer: [{ name: 'stella artois', links: 'productPage.html' }], 
+           };
+
+
+function find(beer, name) {
+    //no such array
+    if(!data[beer])
+        return;
+
+    //search array for key
+    var beer = data[beer];
+    for(var i = 0; i < beer.length; ++i) {
+        //if the name is what we are looking for return it
+        if(beer[i].name === name)
+
+            window.document.location.href = 'productPage.html';
+    }
+}
+document.addEventListener("deviceready", init, false);
+function init() {
+    document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+    resultDiv = document.querySelector("#results");
+}
+
+function startScan() {
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            var s = "Result: " + result.text + "<br/>" +
+            "Format: " + result.format + "<br/>" +
+            "Cancelled: " + result.cancelled;
+            resultDiv.innerHTML = s;
+
+            if (result.text=="786150000052") {
+                window.document.location.href = 'productPage.html';
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+    $.get(
+    "somepage.php",
+    {paramOne : 1, paramX : 'abc'},
+    function(data) {
+       alert('page content: ' + data);
+    }
+    /*function httpGet(https://api.untappd.com/v4/method_name?access_token=ACESSTOKENHERE)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}*/
+);
+
+}
--- www/js/productPage.js
+++ www/js/productPage.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
-  
-$("body").addClass("animated hinge");
-  $('.btn.danger').button('toggle').addClass('fat')
-  
-
-  });
+    var fill = 0;
+    var update = setInterval(function() {
+        fill += 1;
+        $('#fillIcon').css('height', (fill+'%')); 
+        if (fill === 20) {
+            clearInterval(update);        
+        }
+    }, 100);
+    var fill = 0;
+    var update = setInterval(function() {
+        fill += 1;
+        $('#fillIcon').css('height', (fill+'%')); 
+        if (fill === 80) {
+            clearInterval(update);        
+        }
+    }, 100);
